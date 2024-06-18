9e7237fb0d9e5c4e1d6fe49a693860fa9aeb08da
edison0341 <edison0341@gmail.com>
2024-06-18 15:03:51 -0400

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
 * main - Entry point
 *
@@ -26,6 +27,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and less than 6 and not 0\n", n, m);
 	}
-
-		return (0);
+	return (0);
 }
