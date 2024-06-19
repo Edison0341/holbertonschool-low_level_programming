54c22b02ce176dac76ef748331681a053fae1582
edison0341 <edison0341@gmail.com>
2024-06-19 11:56:28 -0400

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is ser appropiately.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
