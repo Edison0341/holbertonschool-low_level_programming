9d3880d2f5cbe72835b7c315c3c690d3b1bcdd2d
edison0341 <edison0341@gmail.com>
2024-06-24 21:03:54 -0400

    Winning is only half of it.

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: variable to use
+ */
+
+void puts_half(char *str)
+{
+	int count, i;
+
+	for (count = 0; count >= 0; count++)
+	{
+		if (str[count] == '\0')
+		{
+			break;
+		}
+	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
