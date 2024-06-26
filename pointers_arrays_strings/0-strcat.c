bc61b6c5386a8adfc0e90736f44a71fb2bab7771
edison0341 <edison0341@gmail.com>
2024-06-26 08:56:44 -0400

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: (0)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	for (lengthD = 0; *(dest + lengthD) != '\0'; lengthD++);
+
+	for (lengthS = 0; *(src + lengthS) != '\0' && lengthD < 97; lengthS++)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+	}
+
+	*(dest + lengthD) = '\0';
+	return (dest);
+}
