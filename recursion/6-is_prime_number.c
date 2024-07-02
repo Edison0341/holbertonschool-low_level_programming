72e381d36ecc9b4fd94e761697f541bb62c0de68
edison0341 <edison0341@gmail.com>
2024-07-01 21:12:18 -0400

    prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * primo - makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: (1)
+ */
+
+int primo(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (primo(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: integer
+ * Return: (1);
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (primo(n, 2));
+}
