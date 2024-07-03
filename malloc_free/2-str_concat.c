2f28eaf3d60d4447efc3cd66e911b89a2c38e2c0
edison0341 <edison0341@gmail.com>
2024-07-03 18:24:06 -0400

    str concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -6,35 +7,29 @@
  * @s2: second string
  * Return: concatenated string
  */
+
 char *str_concat(char *s1, char *s2)
 {
 	int i, j, k, l;
-	char *anostr;
+	char *pn;
 
 	if (s1 == NULL)
 		s1 = " ";
-
 	if (s2 == NULL)
 		s2 = " ";
-
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	anostr = malloc(sizeof(char) * (i + j + 1));
+	pn = malloc(sizeof(char) * (i + j + 1));
 
-	if (anostr == NULL)
+	if (pn == NULL)
 		return (NULL);
 
-	for (k = 0; k < i; k++)
-		anostr[k] = s1[k];
-
-	for (l = 0; l < j; l++, k++)
-		anostr[k] = s2[l];
-
-	anostr[k] = '\0';
-
-	return (anostr);
+	for (k = 0; s1[k] != '\0'; k++)
+		pn[k] = s1[k];
+	for (l = 0; s2[l] != '\0'; l++, k++)
+		pn[k] = s2[l];
+	return (pn);
 }
