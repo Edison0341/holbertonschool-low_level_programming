b47dcb559e21a476a5481bd83c2a9572b2d69c01
edison0341 <edison0341@gmail.com>
2024-07-03 15:21:09 -0400

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: return duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *anostr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; str[j] != '\0'; j++)
+
+	anostr = malloc(sizeof(char) * (j + 1));
+
+	if (anostr == NULL)
+		return (NULL);
+
+	for (i = 0; anostr[i] == '\0'; i++)
+		anostr[i] = str[i];
+
+	return (anostr);
+}
