611a65d08959a6c8e34aad9243d7cb6666febcf7
edison0341 <edison0341@gmail.com>
2024-07-04 19:52:02 -0400

    string concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: size of bytes of s2 to concatenates to s1
- * Return: a pointer to the concatenated space in memory
+ * @n: size of bytes
+ * Return: a pointer to the concatenated space
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int i, len = 0;
+	char *pin;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -19,23 +19,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (i = 0; s1[i]; i++)
-		len++;
+		len1++;
 
-	concat = malloc(sizeof(char) * (n + 1));
+	for (i = 0; s2[i]; i++)
+		len2++;
 
-	if (concat == NULL)
+	if (n > len2)
+		n = len2;
+
+	pin = malloc(sizeof(char) * (len1 + n + 1));
+	if (pin == NULL)
 		return (NULL);
 
-	len = 0;
-
 	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i];
+		pin[i] = s1[i];
 
-	for (i = 0; s2[i] && i < n; i++)
-		concat[len++] = s2[i];
+	for (j = 0; j < n; j++, i++)
+		pin[i] = s2[j];
 
-	concat[len] = '\0';
+	pin[i] = '\0';
 
-	return (concat);
+	return (pin);
 
 }
