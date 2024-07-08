3e06a01383854f84883f9545b34c4efa80b5710b
edison0341 <edison0341@gmail.com>
2024-07-08 11:58:32 -0400

    init_dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,7 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	my_dog.name = "Poppy";
-	my_dog.age = 35;
-	my_dog.owner = "Bob";
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
