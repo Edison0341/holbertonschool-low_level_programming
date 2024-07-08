c16ff15a9502847f716249e9b2915535149230ac
edison0341 <edison0341@gmail.com>
2024-07-08 15:12:59 -0400

    function creates a new dog structures

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * _strdup - duplicate a string
+ * @s: strings to duplicate
+ * Return: pointer to the new string
+ */
+
+char *_strdup(char *s)
+{
+	int i, j;
+	char *str;
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	str = malloc(sizeof(char) * (i + 1));
+
+	for (j = 0; str[j] != '\0'; j++)
+		str[j] = s[j];
+
+	str[j] = '\0';
+
+	return (str);
+}
+
+/**
+ * new_dog - creates a new dog struct
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: name of the owner
+ * Return: pointer to dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = _strdup(name);
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+
+}
+
