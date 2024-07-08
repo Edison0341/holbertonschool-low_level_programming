7c8e0759dec7ed168b1828e173ece8cf38323c4d
edison0341 <edison0341@gmail.com>
2024-07-08 11:20:24 -0400

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,23 @@
-#ifndef "dog.h"
-#define "dog.h"
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - structure for dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
  */
+
 typedef struct dog
 {
-char name;
+char *name;
 float age;
-char owner;
-} my_dog;
+char *owner;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* dog.h */
+#endif /* DOG_H */
+
