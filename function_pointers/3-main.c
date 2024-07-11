fd89bc16bed7492bc8b8c50e9ef7afe92ee91426
edison0341 <edison0341@gmail.com>
2024-07-11 12:18:48 -0400

    pushing all files for 3rd project of calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - this function executes the calculator
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: the result of the operation
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (((*argv[2] == '/') || (*argv[2] == '%')) &&(atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
+	
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sums of the two integer;
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: substraction of the two integer;
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of the two integer;
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sums of the two integer;
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the two integer;
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
