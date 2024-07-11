b216baffa14b496c4377382c06c791833fa4f6ba
edison0341 <edison0341@gmail.com>
2024-07-11 14:22:49 -0400

    changes in main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,14 +12,16 @@
 int main(int argc, char *argv[])
 {
 	int result;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1, num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if (get_op_func(argv[2]) == NULL)
 	{
