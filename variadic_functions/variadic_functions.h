80ee176072f310d69f53bbe5e449a701ab76b294
edison0341 <edison0341@gmail.com>
2024-07-14 21:59:26 -0400

    print all plus upgraded from variadic function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,17 @@
+#include <stdarg.h>
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+typedef struct types {
+    char z;
+    void (*f)(va_list args);
+} types_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void print_float(va_list args);
+void print_string(va_list args);
+void print_char(va_list args);
+void print_int(va_list args);
 #endif /* VARIADIC_FUNCTION_H */
