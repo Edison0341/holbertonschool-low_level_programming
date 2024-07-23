7703d85a7a5eebc9b34371eafd252dbb1f154b09
edison0341 <edison0341@gmail.com>
2024-07-23 15:32:27 -0400

    0 print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,11 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[%d} (nil)\n", 0);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
 	}
 	return (i);
