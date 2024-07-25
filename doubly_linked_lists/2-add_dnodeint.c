ffd6d1da1d5be3fcf61d0f25e87e51c9c6bef951
edison0341 <edison0341@gmail.com>
2024-07-25 18:46:39 -0400

    add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL) 
+	if (new == NULL)
 	{
 		return (NULL);
 	}
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 
-	if (*head != NULL) 
+	if (*head != NULL)
 	{
 		(*head)->prev = new;
 	}
