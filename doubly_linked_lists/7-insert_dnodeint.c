b62218ca249290cd9e0b464b9e1758dae0d5c87e
edison0341 <edison0341@gmail.com>
2024-07-27 14:29:42 -0400

    insert dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,46 +9,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
+	dlistint_t *current = *h;
+	dlistint_t *new;
 
 	if (idx == 0)
 	{
-		new->next = *h;
-		new->prev = NULL;
-		if (*h != NULL)
-		{
-			(*h)->prev = new;
-		}
-		*h = new;
-		return (new);
+		return (add_dnodeint(h, n));
 	}
-	current = *h;
-	while (idx > 1)
+	while (idx > 1 && current != NULL)
 	{
 		current = current->next;
 		idx--;
-		if (current == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
 	}
-
+	if (current == NULL || current->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
 	new->next = current->next;
 	new->prev = current;
-	if (current->next != NULL)
-	{
-		current->next->prev = new;
-	}
-
+	current->next->prev = new;
 	current->next = new;
+
 	return (new);
-	
 }
