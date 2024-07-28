9373bdcf7899ad8f4fd6ea53302e5e771c5c9340
edison0341 <edison0341@gmail.com>
2024-07-28 10:31:20 -0400

    create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,10 +10,12 @@ int create_file(const char *filename, char *text_content)
 	int new_file, len, wr_stat;
 
 	if (filename == NULL)
+		perror("filename is NULL");
 		return (-1);
 
 	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (new_file == -1)
+		perror("Error opening file");
 		return (-1);
 
 	if (text_content != NULL)
@@ -21,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; len++)
 			;
 		wr_stat = write(new_file, text_content, len);
+		perror("Error writing to file");
 		if (wr_stat == -1)
 		{
 			close(new_file);
@@ -28,7 +31,10 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	if (close(new_file) == -1)
+	{
+		perror("Error closing file");
 		return (-1);
+	}
 
 	return (1);
 }
